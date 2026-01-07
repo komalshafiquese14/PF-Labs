3ea40e95c07e703e63a00f7df51499f428c09536
komalshafiquese14 <komalshafiquese14@gmail.com>
2026-01-07 11:47:58 +0500

    Lab 1 & 2

---
+++ LAB 1 and 2/Escap seq 1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{cout<<"Items\t\tPrice\t\tQuality\n"
+<<"Pen\t\t$1\t\t10\n"
+<<"Pencil\t\t$2\t\t5\n"
+<<"Eraser\t\t$0.5\t\t20\n";
+return(0);
+
+	
+}
---
+++ LAB 1 and 2/escape seq 2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+//{
+{
+cout<<"She said,\"Let's learn C++!\"\n"
+<<"To start,save your file in C:\\C++_Projects\\Lab1\n"
+<<"Good Luck!\n";
+return(0);
+
+	
+}
